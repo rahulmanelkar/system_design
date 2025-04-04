8868be9e760025abc13a5eb49e2db1c612a5bf5a
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-04-03 19:52:04 -0700

    working on parking lot

--- parking_lot/pedestrian.py
+++ parking_lot/pedestrian.py
@@ -11,7 +11,7 @@ import tkinter as tk
 def main():
     #This is a pedestrian
     print("This is a pedestrian")
-    print("Pedes")
+    print("Pedestrian")
 
 if __name__ == '__main__':
     main()
